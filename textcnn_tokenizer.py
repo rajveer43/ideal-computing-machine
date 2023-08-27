f6b2c3c503418d74d784a125884e3d2ab48e5db7
rajveer43 <rajveer.rathod1301@gmail.com>
2023-08-27 22:30:44 +0530

    Update textcnn_tokenizer.py

--- textcnn_tokenizer.py
+++ textcnn_tokenizer.py
@@ -8,6 +8,19 @@ from config import inputLen
 
 
 def train_tokenizer(train_datas, test_datas, tokenizer_file_path):
+    """
+    Train a tokenizer on training and test data and save it to a file.
+
+    Args:
+        train_datas (list): List of training data samples as strings.
+        test_datas (list): List of test data samples as strings.
+        tokenizer_file_path (str): Path to save the trained tokenizer as a pickle file.
+
+    Returns:
+        Tokenizer: Trained tokenizer.
+        numpy.ndarray: Padded and tokenized training data.
+        numpy.ndarray: Padded and tokenized test data.
+    """
     tokenizer = Tokenizer(
         num_words=None,
         filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\t\n',
@@ -40,6 +53,21 @@ def train_tokenizer(train_datas, test_datas, tokenizer_file_path):
 def train_tokenizer_with_val(
     train_datas, val_datas, test_datas, tokenizer_file_path
 ):
+    """
+    Train a tokenizer on training, validation, and test data and save it to a file.
+
+    Args:
+        train_datas (list): List of training data samples as strings.
+        val_datas (list): List of validation data samples as strings.
+        test_datas (list): List of test data samples as strings.
+        tokenizer_file_path (str): Path to save the trained tokenizer as a pickle file.
+
+    Returns:
+        Tokenizer: Trained tokenizer.
+        numpy.ndarray: Padded and tokenized training data.
+        numpy.ndarray: Padded and tokenized validation data.
+        numpy.ndarray: Padded and tokenized test data.
+    """
     tokenizer = Tokenizer(
         num_words=None,
         filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\t\n',
